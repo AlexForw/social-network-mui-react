ff5fd6827008e8c8ae20f01c805b0505088d146d
AlexForw <lmityushkin@mail.ru>
2022-04-19 16:29:02 +0300

    feat: add material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css'
+import { Button, styled, Typography } from '@mui/material'
+import { Settings } from '@mui/icons-material'
+import { fontSize } from '@mui/system';
 
 function App() {
-
 
   return (
     <div className="App">
