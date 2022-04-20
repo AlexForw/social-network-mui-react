46a47c05f0eef64e859990a839c8cbca1d523a4d
AlexForw <lmityushkin@mail.ru>
2022-04-20 21:29:40 +0300

    fix: clean ListWrapper

--- src/components/assets/ListWrapper.jsx
+++ src/components/assets/ListWrapper.jsx
@@ -1,14 +1,9 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import ListSocial from './ListSocial';
-
 import Collapse from '@mui/material/Collapse';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import StarBorder from '@mui/icons-material/StarBorder';
 
 
 
