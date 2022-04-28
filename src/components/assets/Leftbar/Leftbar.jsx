46ad1a14fd39f0c755d0345d9ba8fa47e1380bff
AlexForw <lmityushkin@mail.ru>
2022-04-28 19:42:28 +0300

    feat: change NavLink to NavLink

--- src/components/assets/Leftbar/Leftbar.jsx
+++ src/components/assets/Leftbar/Leftbar.jsx
@@ -6,7 +6,7 @@ import NewspaperIcon from '@mui/icons-material/Newspaper';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import GroupIcon from '@mui/icons-material/Group';
 import GroupsIcon from '@mui/icons-material/Groups';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Leftbar = ({ sx }) => {
@@ -14,21 +14,25 @@ const Leftbar = ({ sx }) => {
         <Box flex={1} p={2} sx={sx}>
             <Box >
                 <Box position='fixed'>
-                    <Link to='/MyProfile'>
-                        <ListSocial href='#' icon={<AccountBoxIcon />} primary='My profile'></ListSocial>
-                    </Link>
-                    <Link to='/'>
-                        <ListSocial href='#' icon={<NewspaperIcon />} primary='News'></ListSocial>
-                    </Link>
-                    <Link to='/messenger'>
-                        <ListSocial href='#' icon={<ChatBubbleOutlineIcon />} primary='Messenger'></ListSocial>
-                    </Link>
-                    <Link to='/friends'>
-                        <ListSocial href='#' icon={<GroupIcon />} primary='Friends'></ListSocial>
-                    </Link>
-                    <Link to='/groups'>
-                        <ListSocial href='#' icon={<GroupsIcon />} primary='Groups'></ListSocial>
-                    </Link>
+                    <NavLink to='/myprofile'>
+                        <ListSocial icon={<AccountBoxIcon />} primary='My profile'></ListSocial>
+                    </NavLink>
+
+                    <NavLink to='/'>
+                        <ListSocial icon={<NewspaperIcon />} primary='News'></ListSocial>
+                    </NavLink>
+                    
+                    <NavLink to='/messenger'>
+                        <ListSocial icon={<ChatBubbleOutlineIcon />} primary='Messenger'></ListSocial>
+                    </NavLink>
+
+                    <NavLink to='/friends'>
+                        <ListSocial icon={<GroupIcon />} primary='Friends'></ListSocial>
+                    </NavLink>
+
+                    <NavLink to='/groups'>
+                        <ListSocial icon={<GroupsIcon />} primary='Groups'></ListSocial>
+                    </NavLink>
                 </Box>
             </Box>
         </Box>
