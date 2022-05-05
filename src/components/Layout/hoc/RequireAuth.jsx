87d1322b44cd97d497607e43c6092836a3949e3f
AlexForw <lmityushkin@mail.ru>
2022-05-05 14:40:55 +0300

    feat: add upload feature

--- src/components/Layout/hoc/RequireAuth.jsx
+++ src/components/Layout/hoc/RequireAuth.jsx
@@ -3,11 +3,24 @@ import useAuth from "../../assets/hooks/useAuth";
 
 
 const RequireAuth = () => {
+    let storage
     const location = useLocation()
     const { authUser } = useAuth()
 
+    if(localStorage.getItem('storage') === null) {                         // If values from key 'todo' is empty
+        storage = []                                                       // Create array 'todo'
+    }else{
+        storage = JSON.parse(localStorage.getItem('storage'))                 // Rewrite array in 'todo' from local storage
+    }
+
+    console.log(storage);
+    
+    if(authUser?.user?._tokenResponse?.email){
+        storage.push(authUser?.user?._tokenResponse?.email)
+        localStorage.setItem('storage',JSON.stringify(storage))
+    }
     return (
-        authUser?.user?._tokenResponse?.email ? <Outlet /> : <Navigate to='/login' state={{ from: location }} replace />
+        JSON.parse(localStorage.getItem('storage')) ? <Outlet /> : <Navigate to='/login' state={{ from: location }} replace />
     )
 }
 export default RequireAuth;
